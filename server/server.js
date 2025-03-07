6553e7c40825cded842358311d71de98701d9aea
Kkaif4 <120726421+Kkaif4@users.noreply.github.com>
2025-03-07 21:39:59 +0530

    Update server.js
    Middleware updatation

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import planRoutes from "./routes/planRoutes.js";
@@ -11,6 +12,7 @@ connectDB();
 
 //Middleware
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
