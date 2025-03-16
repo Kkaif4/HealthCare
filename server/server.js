015bc45b4a816a744cb583a751bc1b7d285aa8a2
Kkaif4 <120726421+Kkaif4@users.noreply.github.com>
2025-03-16 23:21:40 +0530

    final push

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import planRoutes from "./routes/planRoutes.js";
@@ -9,6 +11,8 @@ import { errorHandler } from "./middlewares/errorMW.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 connectDB();
 
 //Middleware
@@ -17,13 +21,23 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Handle wrong API requests
+app.use((req, res) => {
+  if (req.accepts("html")) {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+  } else {
+    res
+      .status(404)
+      .json({ success: false, message: "API not found or wrong API request" });
+  }
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
