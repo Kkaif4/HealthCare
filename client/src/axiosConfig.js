ce5132fe1ca500f0a9962115d0680cb5310243d1
Kkaif4 <120726421+Kkaif4@users.noreply.github.com>
2025-09-08 18:01:48 +0530

    fix: updating backend url

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://healthcare-kafv.onrender.com.api';
+  axios.defaults.baseURL = 'https://healthcare-1-zl38.onrender.com/api';
 } else {
   axios.defaults.baseURL = 'http://localhost:5000/api';
 }
